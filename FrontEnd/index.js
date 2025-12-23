aa4c3ac9469d8a96d40159c348835802d264b7f0
Viknesh <sadishviknesh@gmail.com>
2025-12-23 16:49:23 +0100

    Ajout des filtres et works via le Backend

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,11 +1,51 @@
+
+//////////// Fetch ////////////
+
+
 async function afficherCategories() {
   const response = await fetch ("http://localhost:5678/api/categories")
   const categories = await response.json()
+
+    const button = document.createElement ("button")
+    button.textContent = "Tous"
+    button.addEventListener("click", function(){
+    })
+    const filtres = document.getElementById ("filtres")
+    filtres.appendChild(button)
+
+  categories.forEach(categorie => {
+    const button = document.createElement ("button")
+    button.textContent = categorie.name
+    button.addEventListener("click", function() {
+    })
+    filtres.appendChild(button)
+    
+  })
+
   console.log(categories)
 }
+afficherCategories()
+
 
 async function afficherWorks() {
   const response = await fetch ("http://localhost:5678/api/works")
   const works = await response.json()
+
+  works.forEach(work => {
+    const figure = document.createElement("figure")
+    const img = document.createElement ("img")
+    img.src = work.imageUrl
+    const figcaption = document.createElement ("figcaption")
+    figcaption.textContent = work.title
+    const gallery = document.getElementById("gallery")
+    figure.appendChild(img)
+    figure.appendChild(figcaption)
+    gallery.appendChild(figure)
+    
+  }); 
+
   console.log(works)
 }
+afficherWorks()
+
+
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -4,14 +4,14 @@
 // let body = document.querySelector("body")
 // body.appendChild(button)
 
-let html = `
-    <nav class="buttons-container">
-			<ul class="buttons-filtre">
-				<li><button class="button-change">Tous</button></li>
-				<li><button class="button-change">Objets</button></li>
-				<li><button class="button-change">Appartements</button></li>
-				<li><button class="button-change" >Hotels & restaurants</button></li>
-			</ul>
-		</nav>
-`
-document.getElementById("filtres").innerHTML = html;
+// let html = `
+//     <nav class="buttons-container">
+// 			<ul class="buttons-filtre">
+// 				<li><button class="categories">Tous</button></li>
+// 				<li><button class="categories">Objets</button></li>
+// 				<li><button class="categories">Appartements</button></li>
+// 				<li><button class="categories">Hotels & restaurants</button></li>
+// 			</ul>
+// 		</nav>
+// `
+// document.getElementById("filtres").innerHTML = html;
